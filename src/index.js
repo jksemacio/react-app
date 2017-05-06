64a4a3069f084524ce89655ab03b36f600b2a141
jksemacio <jksemacio@gmail.com>
2017-05-06 21:59:11 +0800

    clock class component with state and lifecycle hooks

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Clock(props) {
-    return (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {props.date.toLocaleTimeString()}.</h2>
-        </div>
-    );
-};
-function tick() {
-    ReactDom.render(
-    <Clock date={new Date()} />,
-    document.getElementById('root')
-    );
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    };
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    };
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    };
+
+    tick() {
+        this.setState({
+                date: new Date()
+            });
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    };
 };
 
-setInterval(tick, 1000);
+
+ReactDom.render(
+    <Clock />,
+    document.getElementById('root')
+);
