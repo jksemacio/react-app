d7bc4dcff210274e7767c69cc4e80805f49e3b6c
jksemacio <jksemacio@gmail.com>
2017-05-08 01:03:25 +0800

    use instead of loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,18 +12,18 @@ const config = {
             { 
                 test: /\.js$/, 
                 exclude: /node_modules/,
-                loader: 'babel-loader'
+                use: 'babel-loader'
             },
             {
                 test: /\.css$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    loader: 'css-loader'
+                    use: 'css-loader'
                 })
             },
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
-                loaders: [
+                use: [
                     'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
                     'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
                 ]
