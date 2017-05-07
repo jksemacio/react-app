6e0b6d02638cf7e28489fc729b64f47d5fa46bfc
jksemacio <jksemacio@gmail.com>
2017-05-08 02:53:58 +0800

    import small jpg file

--- src/image-viewer.js
+++ src/image-viewer.js
@@ -1,7 +1,7 @@
 import "../styles/image-viewer.css";
-import big from "../assets/big.jpg";
+import small from "../assets/small.jpg";
 
 const image = document.createElement('img');
-image.src = big;
+image.src = small;
 
 document.body.appendChild(image);
