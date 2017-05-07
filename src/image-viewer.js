6eea28a86a367f4c317e07573178ab03e834ab3c
jksemacio <jksemacio@gmail.com>
2017-05-07 23:40:23 +0800

    import small jpg and use as src in image

--- src/image-viewer.js
+++ src/image-viewer.js
@@ -1,6 +1,7 @@
 import "../styles/image-viewer.css";
+import small from "../assets/small.jpg";
 
 const image = document.createElement('img');
-image.src = "http://lorempixel.com/20/20";
+image.src = small;
 
 document.body.appendChild(image);
