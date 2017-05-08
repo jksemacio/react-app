5bd4bf6e932981fae9d0d72ed1523145c2363d3b
jksemacio <jksemacio@gmail.com>
2017-05-08 13:45:20 +0800

    structure

--- src/image-viewer.js
+++
@@ -1,7 +0,0 @@
-import "../styles/image-viewer.css";
-import small from "../assets/small.jpg";
-
-const image = document.createElement('img');
-image.src = small;
-
-document.body.appendChild(image);
