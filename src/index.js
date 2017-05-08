58e506440b7ea5f6a37ddb443098873310b68047
jksemacio <jksemacio@gmail.com>
2017-05-09 01:00:23 +0800

    react with JSX

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  React.createElement('h1', null, 'Hello React!'),
+  <h1>Hello React with JSX</h1>,
   document.getElementById('root')
 );
