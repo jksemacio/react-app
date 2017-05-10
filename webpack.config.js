b54633ca2ac9e5a745341a7beb916ae946fbe146
jksemacio <jksemacio@gmail.com>
2017-05-10 19:14:22 +0800

    ejs-html-loader?initialData=&initialMarkup=

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ const config = {
             },
             {
                 test: /\.ejs$/,
-                use: ['ejs-loader','ejs-html-loader?initialMarkup=Hello%20EJS!']
+                use: ['ejs-loader','ejs-html-loader?initialData=&initialMarkup=']
             }
         ]
     },
